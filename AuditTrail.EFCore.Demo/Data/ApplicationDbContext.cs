8589277e409e4902c2c6eb35ba1ca17051a8fb46
Clive Harding <clive.harding@daimler.com>
2022-01-27 14:14:26 +0200

    Add project files.

---
+++ AuditTrail.EFCore.Demo/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+namespace AuditTrail.EFCore.Demo.Data
+{
+    public class ApplicationDbContext : AuditableIdentityContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Models.Product> Product { get; set; }        
+    }
+}
